c64242524ef152024ca237f9a8f6654338d55c9a
Daniel120904 <danieladriano1209@gmail.com>
2024-10-24 18:41:49 -0300

    :)

--- src/main/java/fatec/sigafx/controller/ProfessorController.java
+++ src/main/java/fatec/sigafx/controller/ProfessorController.java
@@ -3,8 +3,47 @@ package fatec.sigafx.controller;
 import fatec.sigafx.view.LoginView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 
 public class ProfessorController {
+
+    @FXML
+    private VBox mainContent;
+
+    @FXML
+    private VBox turma;
+
+    @FXML
+    private VBox presenca;
+
+    // Esconder todos os painéis
+    private void esconderPaineis() {
+        mainContent.setVisible(false);
+        turma.setVisible(false);
+        presenca.setVisible(false);
+    }
+
+    // Mostrar "Início"
+    @FXML
+    public void mostrarInicio() {
+        esconderPaineis();
+        mainContent.setVisible(true);
+    }
+
+    // Mostrar *Turma*
+    @FXML
+    public void mostrarTurma() {
+        esconderPaineis();
+        turma.setVisible(true);
+    }
+
+    // Mostrar *Precença*
+    @FXML
+    public void mostrarPrecenca() {
+        esconderPaineis();
+        presenca.setVisible(true);
+    }
+
     @FXML
     public void onLogoutClicked(ActionEvent event) {
         // Redireciona para a página de login
