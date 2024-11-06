05eff538227c4b304e2d3b1bcccc218c080a6e1f
Matheus Graciliano <matheus.kkg21@gmail.com>
2024-11-06 18:36:57 -0300

    feat: redirecionamento apos o login

--- src/main/java/fatec/sigafx/controller/LoginController.java
+++ src/main/java/fatec/sigafx/controller/LoginController.java
@@ -1,14 +1,19 @@
 package fatec.sigafx.controller;
 
 import fatec.sigafx.dao.UsuarioDAO;
+import fatec.sigafx.model.admin.AdminModel;
+import fatec.sigafx.model.aluno.AlunoModel;
+import fatec.sigafx.model.professor.ProfessorModel;
 import fatec.sigafx.model.usuario.UsuarioModel;
-import fatec.sigafx.view.HomeView;
+import fatec.sigafx.view.AdmView;
+import fatec.sigafx.view.AlunoView;
+import fatec.sigafx.view.ProfessorView;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-public class UsuarioController {
+public class LoginController {
     private static final UsuarioDAO dao = new UsuarioDAO();
 
     @FXML
@@ -22,14 +27,23 @@ public class UsuarioController {
 
     @FXML
     private void confirmarLogin() {
+
+        //TODO: mover validação para o Model
         UsuarioModel u = dao.buscarPorNome(usuarioNome.getText());
         if (u != null && u.getSenha().equals(usuarioSenha.getText())) {
-            HomeView.mostrarHome();
-        }
-        else {
-            //Exibir erro caso o login falhe
+            if (u instanceof AdminModel) {
+                AdmView.mostraAdm();
+            }
+
+            if (u instanceof AlunoModel) {
+                AlunoView.mostraAluno();
+            }
+
+            if (u instanceof ProfessorModel) {
+                ProfessorView.mostraProf();
+            }
+        } else {
             mensagemErroLogin.setVisible(true);
         }
-
     }
 }
